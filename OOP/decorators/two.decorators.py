794deb9fac7353d1cb44a9443d09f3b7e39ca3f1
masinde70 <masinde.masinde@metropolia.fi>
2018-02-26 15:22:40 +0200

    Wrapper factory

---
+++ OOP/decorators/two.decorators.py
@@ -0,0 +1,27 @@
+from time import sleep, time
+from functools import wraps
+
+def measure(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        t = time()
+        result = func(*args, **kwargs)
+        print(func.__name__, 'took:', time() -t)
+        return results
+    return wrapper
+
+def max_result(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if result > 100:
+            print ('Result is too big ({0}). Max allowed is 100.' .format(result))
+            return results
+        return wrapper
+
+@measure
+@max_result
+def cube(n):
+    return n ** 3
+print(cube(2))
+print(cube(5))
