f9f2230eaec7eddae00a97d47855b3b6c55f2d50
masinde70 <masinde.masinde@metropolia.fi>
2018-02-26 15:26:48 +0200

    Wrapper Factory

--- OOP/decorators/decorators.factory.py
+++ OOP/decorators/decorators.factory.py
@@ -9,7 +9,7 @@ from functools import wraps
 
 def max_result(threshold):
     def decorator(func):
-        @wraps(func):
+        @wraps(func)
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
             if result > threshold:
