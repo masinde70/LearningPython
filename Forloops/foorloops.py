6857bec7b0fd923adaf4aaf9825a6f51c02e9f82
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-03-28 14:51:05 +0300

    loops

--- Forloops/foorloops.py
+++ Forloops/foorloops.py
@@ -6,8 +6,15 @@
 #     for j in range(len(interior)):
 #         print(interior[j])
 
-st = "hello world"
+# st = "hello world"
 
-for i, char in enumerate(st):
-    if char == 'w':
-        print(i)
+# for i, char in enumerate(st):
+#     if char == 'w':
+#         print(i)
+
+numbers = []
+for i in range(10):
+    num = input("Enter a number: ")
+    numbers.append(num)
+
+print(numbers)
