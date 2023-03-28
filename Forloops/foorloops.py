98f80bec1e1bfe088c91655c5b9ae7ea46a932ae
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-03-28 14:46:10 +0300

    loops

--- Forloops/foorloops.py
+++ Forloops/foorloops.py
@@ -1,7 +1,13 @@
-lst = [[1,2], [3,4],[5,6]]
+# lst = [[1,2], [3,4],[5,6]]
 
-for i in range(len(lst)):
-    interior = lst[i]
+# for i in range(len(lst)):
+#     interior = lst[i]
 
-    for j in range(len(interior)):
-        print(interior[j])
+#     for j in range(len(interior)):
+#         print(interior[j])
+
+st = "hello world"
+
+for i, char in enumerate(st):
+    if char == 'w':
+        print(i)
