9d812e8604d21051ebc8f78640150cce3ed05476
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-03-28 12:23:41 +0300

    loops

--- Forloops/foorloops.py
+++ Forloops/foorloops.py
@@ -1,4 +1,8 @@
 lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, False, True, False, False]
-
-for i, element in enumerate(lst):
+tup = (1, 2, 3, 4, 5, 6, 7, "masinde", 45)
+for i, element in enumerate(tup):
+   if element == "masinde" or element == 7:
+      break
    print(i, element)
+
+print("Done!")
