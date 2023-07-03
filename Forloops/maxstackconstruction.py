2c106744bfb416fd2a94682ccf0af3ddb360f06e
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-03 10:46:13 +0300

    minmax_stack_constructions

--- Forloops/maxstackconstruction.py
+++ Forloops/maxstackconstruction.py
@@ -1,4 +1,8 @@
 class MinMaxStack:
     def __init__(self):
        self.minMaxStack = []
-        
+       self.stack = []
+    #O(1) time | O(1) space 
+    def peek(self):
+        return self.stack[len(self.stack)-1]
+    
