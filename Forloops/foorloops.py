3fdf3afa30b92f93a3c1c66536ad3c1b9493086e
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-03 10:24:31 +0300

    stack construction

--- Forloops/foorloops.py
+++ Forloops/foorloops.py
@@ -18,5 +18,14 @@ for i in range(10):
     numbers.append(num)
 
 print(numbers)
+# use a single loop to iterate through the list and print that are both divisble by 2 and located at an odd index, each on separate lines line
+lst = [45, 24, 22, 1, 45, 2, 12, 13, 16, 10, 0, -7]
+
+# Write your code here.
+for i in range(len(lst)):
+    item = lst[i]
+
+    if item % 2 == 0 and i % 2 != 0:
+        print(item)
 
 
--- Forloops/tuple.py
+++ Forloops/tuple.py
@@ -1,4 +1,13 @@
-x = (1,2,3)
+z = (1,0,3)
+count = z.count(2)
 
+x = (1, 3)
+y = (5, 56)
+
+combined = x*y
+print(combined)
+contains = 0 in x 
+print(contains)
+print (count)
 for i in (x):
     print(i)
