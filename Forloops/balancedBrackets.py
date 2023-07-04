3cada5f41e39e70e21671e5af00708569df3ea67
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-04 12:43:38 +0300

    balanced brackets

--- Forloops/balancedBrackets.py
+++ Forloops/balancedBrackets.py
@@ -2,3 +2,17 @@ def balancedBrackets(string):
     openBrackets = "([{"
     closingBrackets = ")]}"
     matchingBrackets = {")": "(", "[" :"]", "}":"{"}
+    stack = []
+
+    for char in string:
+        if char in openBrackets:
+            stack.append(char)
+        elif char in closingBrackets:
+            if len(stack) == 0:
+                return False
+            if stack[-1] == matchingBrackets[char]:
+                stack.pop()
+            else:
+                return False
+    return len(stack) == 0
+
