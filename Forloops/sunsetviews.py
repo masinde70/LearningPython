9c4abe9bdd236f4903bbd234f92569b379384513
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-05 09:53:32 +0300

    balanced brackets

--- Forloops/sunsetviews.py
+++ Forloops/sunsetviews.py
@@ -2,4 +2,9 @@ def sunsetViews(buildings, direction):
     candidateBuildings = []
 
     startIdx = 0 if direction == "EAST" else len(buildings) - 1
-    
+    step = 1 if direction == "EAST" else -1
+
+    idx = startIdx
+    while idx > 0 and idx < len(buildings):
+        buildingHeight = buildings[idx]
+
