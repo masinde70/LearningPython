73a329d67745b436c9d58aac68074e452d1167ed
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-06 08:03:50 +0300

    balanced brackets

--- Forloops/BestDigits.py
+++ Forloops/BestDigits.py
@@ -1,5 +1,8 @@
 """
 Best Digits 
 Write the function that takes a positive integer represented as a string number
-and an integer numDigits. Remove numDigts from the string
+and an integer numDigits. Remove numDigts from the string sot the number represented by the string is large as possible afterwards.
+
+Note that the order of the remaining digits can not be changed. You can assume
+numDigitswill always be be less than the length of number and greater than or equal to 0
 """
