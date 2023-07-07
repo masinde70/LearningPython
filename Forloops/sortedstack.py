6efea29ca1adc0c9ec10d7c63827508f611e9957
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-07 13:50:30 +0300

    sort stack

---
+++ Forloops/sortedstack.py
@@ -0,0 +1,8 @@
+"""Write a function that takes in an array of integers representing a stack, recursively sorts the stack in place
+  (i.e doesn't create new array), and returns it.
+  The array must be treated as stack, with the end of the array as the top of the stack. Therefore, you are only allowed
+  to
+  1. Pop elements from the stack by removing elements to the end of the array using the built-in .pop()
+  2. Push elements to the top of the stack by appending elements to the end of the array using the built-in .append()
+  3. Peek at the element on the top of the stack by accessing the last element in the array.
+ """
