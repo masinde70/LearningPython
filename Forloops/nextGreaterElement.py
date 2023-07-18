3b2187af3df0be766865eaa3149671aa0576723d
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-18 08:43:54 +0300

    next greater element

--- Forloops/nextGreaterElement.py
+++ Forloops/nextGreaterElement.py
@@ -9,6 +9,7 @@ In other words, the function should return a new array where
 outputArray[i] is the next element in the input array that is greater
 than inputArray[i]. If there's no such next greater element for particular index,
 the value at that particular index in the output array should be
--1. For example, given
+-1. For example, given array = [1, 2], your function should
+return [2, -1].
 
 """
