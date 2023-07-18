506dd716aef3b6ab2ddee508ad1f10ffb0aa377f
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-18 13:54:23 +0300

    colliding asteroids

---
+++ Forloops/collidingAsteroids.py
@@ -0,0 +1,14 @@
+"""
+You're given an array of integers asteroids, where each integer
+represents the size of an asteroid. The sign of the intger
+represents the direction the asteroid is moving(positive=right, negative=lef).
+
+For example, the integer 4 represents an asteroid moving to the right. Similarly, -7
+represent an asteroid moving to the left.
+
+If two asteroids collide, the smaller asteroid(based on the absolute value) explodes. If two colliding asteroids
+are the same size, they both explode.
+
+write a function that takes in this array of asteroids and returns an array of integers representing the asteroids after all
+collisions occur.
+"""
