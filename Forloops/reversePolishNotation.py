90a52ec5a8ac13114dad5ccd26f0c1556836af23
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-07-18 09:11:42 +0300

    reverse polish operations

--- Forloops/reversePolishNotation.py
+++ Forloops/reversePolishNotation.py
@@ -7,4 +7,7 @@ def reversePlishNotation(tokens):
     where n is the number of tokens
     """
     stack = [] # Empty stack approach
+    # Observe that all operations in the dictionary below are binary
+    # {"operation": lambda functional operation}
+
     
