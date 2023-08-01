12ed621034e03513211e66b091f5c65a1277b413
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-08-01 12:22:10 +0300

    sorted array

--- Forloops/winner.py
+++ Forloops/winner.py
@@ -3,5 +3,12 @@ Tournament  winner
 
 There's an algorithms tournament taking place in which teams of programmers
 compete against each other to solve algorithimic problems as 
-faster as possible.
+faster as possible. Teams compete in a round robin, where
+each team faces off against all other teams. Only two teams compete
+against each other at a time, and for each competition,
+one team is designated the home team, while the other team is the away team.
+
+In each competition there's always one winner and one lose;
+ther are no ties. A team receives 3 points if it wins and 0 if it loses.
+The winner of the tournament is the team that recieves the most amount of points
 """
