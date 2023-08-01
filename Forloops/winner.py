696cca9b546c26cfb26aa68fe86e48924b0e8618
masinde70 <5812481+masinde70@users.noreply.github.com>
2023-08-01 12:15:58 +0300

    sorted array

--- Forloops/winner.py
+++ Forloops/winner.py
@@ -1,5 +1,7 @@
 """
 Tournament  winner
 
-There's an algorithms tournament taking place
+There's an algorithms tournament taking place in which teams of programmers
+compete against each other to solve algorithimic problems as 
+faster as possible.
 """
